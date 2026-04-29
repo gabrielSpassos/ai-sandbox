5cda5e4b956e9d86649e4e0fc537e14e2eb55b9b
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-29 00:07:05 -0300

    feat: update rust distilation poc

--- rust-distillation/src/training/trainer.rs
+++ rust-distillation/src/training/trainer.rs
@@ -1,12 +1,12 @@
-use tch::{Tensor, nn, nn::OptimizerConfig, Device};
 use anyhow::Result;
+use tch::{nn, nn::OptimizerConfig, Kind};
 
 pub fn train(
     config: &crate::config::Config,
     teacher: &tch::CModule,
     student: &mut crate::models::student::Student,
     mut dataloader: crate::data::dataloader::DataLoader,
-) -> anyhow::Result<()> {
+) -> Result<()> {
 
     let mut opt = nn::Adam::default().build(&student.vs, config.learning_rate)?;
 
@@ -17,6 +17,9 @@ pub fn train(
 
         let mut total_loss = 0.0;
         let mut batches = 0;
+
+        let mut total_correct = 0.0;
+        let mut total_samples = 0.0;
 
         while let Some((images, labels)) = dataloader.next_batch() {
 
@@ -36,13 +39,24 @@ pub fn train(
             total_loss += loss.double_value(&[]);
             batches += 1;
 
+            // Accuracy calculation
+            let preds = student_logits.argmax(-1, false);
+            let correct = preds
+                .eq_tensor(&labels)
+                .to_kind(Kind::Float)
+                .sum(Kind::Float);
+
+            total_correct += correct.double_value(&[]);
+            total_samples += labels.size()[0] as f64;
+
             opt.backward_step(&loss);
         }
 
         println!(
-            "Epoch {} avg loss: {:.4}",
+            "Epoch {} avg loss: {:.4} | accuracy: {:.2}%",
             epoch,
-            total_loss / batches as f64
+            total_loss / batches as f64,
+            100.0 * total_correct / total_samples
         );
     }
 
